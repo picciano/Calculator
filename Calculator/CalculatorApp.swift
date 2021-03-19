c27fc18a4296aa9bb22c9c819abcfac59a40f026
Sir Anthony Picciano <tony.picciano@starz.com>
2021-03-19 14:26:02 -0600

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Anthony Picciano on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
